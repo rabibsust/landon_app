3c377a5ab0805b73fe77e3b5b4930a3d9cedb231
rabib <rabib.sust@gmail.com>
2018-07-09 15:38:03 +0600

    Unit test with phpunit

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Title;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testTitlesModelCount()
+    {
+        $title = new Title();
+        $this->assertTrue( count( $title->all()) === 6, 'There should be 6 titles');
+    }
+    public function testLastTitle()
+    {
+        $title = new Title();
+        $title_array = $title->all();
+        $this->assertEquals('Professor', array_pop($title_array), 'Titles last element should be professor.');
+    }
+}
